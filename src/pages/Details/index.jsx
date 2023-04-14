0232252faebcfe4a61f147239816c0af3a7ddd23
richardmezzomo <richardmezzomo1@gmail.com>
2023-04-14 17:27:36 -0300

    first commit

---
+++ src/pages/Details/index.jsx
@@ -0,0 +1,11 @@
+import { Container } from './styles'
+
+
+export function Details() {
+  return (
+    <Container>
+    <h1>Hello WORLD!</h1>
+    <span>Richard Mezzomo</span>
+    </Container>
+  )
+}
---
+++ src/styles/theme.js
@@ -0,0 +1,15 @@
+export default {
+    COLORS: {
+      BACKGROUND_900: "#0D0C0F",
+      BACKGROUND_800: "#1C1B1E",
+      BACKGROUND_700: "#262529",
+  
+      WHITE: "#F4EDE8",
+  
+      GRAY_100: "#948F99",
+      GRAY_300: "#282124",
+  
+      PINK: "#FF859B",
+      PINK_300: "rgba(255, 133, 155, 0.05)"
+    }
+  }
